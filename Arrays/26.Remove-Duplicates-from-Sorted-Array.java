5d524774f97069890f8d74a3c82a080f4e2075a7
atsogt <atsogt17@gmail.com>
2024-10-17 14:08:19 -0400

    two pointer with psuedocode

--- Arrays/26.Remove-Duplicates-from-Sorted-Array.java
+++ Arrays/26.Remove-Duplicates-from-Sorted-Array.java
@@ -1,11 +1,17 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-
+        //pointer one:
         int j = 1;
 
         for(int i = 1; i < nums.length; i++) {
+          //pointer two: nums[i]
+          //temporary pointer: nums[i - 1]
+
+          //comparing current element to previous element
             if(nums[i] != nums[i - 1]) {
+              //if current element is unique, it is assigned in the array IN-PLACE
                 nums[j] = nums[i];
+              //pointer one get incremented
                 j++;
             }
         }
