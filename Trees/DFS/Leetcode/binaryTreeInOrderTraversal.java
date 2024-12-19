d1ecf63ee6def32aa9d1f935945a8bdfaadd03d8
atsogt <atsogt17@gmail.com>
2024-12-19 06:49:45 -0500

    recursive explanation implemented

--- Trees/DFS/Leetcode/binaryTreeInOrderTraversal.java
+++ Trees/DFS/Leetcode/binaryTreeInOrderTraversal.java
@@ -23,20 +23,22 @@ public class binaryTreeInOrderTraversal {
     }
   }
 
+  // Inorder: left node -> parent node -> right node
   public List<Integer> inorderTraversal(TreeNode root) {
     List<Integer> arr = new ArrayList<>();
-
     inorder(root, arr);
     return arr;
   }
 
   public void inorder(TreeNode root, List<Integer> arr) {
-
+    // recursive base case if root is null, pop from stack
     if (root == null)
       return;
-
+    // recursively traver to the left side
     inorder(root.left, arr);
+    // add to list
     arr.add(root.val);
+    // recursively traver to the right side
     inorder(root.right, arr);
   }
 }
