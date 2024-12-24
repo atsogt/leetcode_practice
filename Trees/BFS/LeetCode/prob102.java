3e8d7cd016ed5855f9be526ffb2bff32b67eb875
atsogt <atsogt17@gmail.com>
2024-12-23 21:03:00 -0500

    intuition explained

--- Trees/BFS/LeetCode/prob102.java
+++ Trees/BFS/LeetCode/prob102.java
@@ -22,12 +22,18 @@ Output: []
 public class prob102 {
   public List<List<Integer>> levelOrder(TreeNode root) {
     List<List<Integer>> res = new ArrayList<>();
+    // each node gets added, then popped off, then children added (if they exist)
     Queue<TreeNode> q = new LinkedList<>();
     if (root != null)
       q.add(root);
     while (!q.isEmpty()) {
+      // each level in tree has it's own array
       List<Integer> arr = new ArrayList<>();
+      // queue size needs to be added or else it will incrase as you add more child
+      // nodes
       int qSize = q.size();
+      // iterate through queue while removing node and then adding child node to queue
+      // for processing
       for (int i = 0; i < qSize; i++) {
         TreeNode curr = q.remove();
         if (curr.left != null) {
@@ -36,8 +42,10 @@ public class prob102 {
         if (curr.right != null) {
           q.add(curr.right);
         }
+        // value of node is added at the end of iteration... from left -> right
         arr.add(curr.val);
       }
+      // each array is added as we progress deeper into tree level
       res.add(arr);
     }
     return res;
