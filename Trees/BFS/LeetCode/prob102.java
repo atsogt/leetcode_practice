2f6e5339a483de49d315acf34059eb01037a41a5
atsogt <atsogt17@gmail.com>
2024-12-23 20:57:25 -0500

    problem prompt added

--- Trees/BFS/LeetCode/prob102.java
+++ Trees/BFS/LeetCode/prob102.java
@@ -6,6 +6,19 @@ import java.util.List;
 import java.util.Queue;
 
 //102. Binary Tree Level Order Traversal
+// https://leetcode.com/problems/binary-tree-level-order-traversal/description/
+
+/*  Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+
+Input: root = [3,9,20,null,null,15,7]
+Output: [[3],[9,20],[15,7]]
+
+Input: root = [1]
+Output: [[1]]
+
+Input: root = []
+Output: []
+*/
 public class prob102 {
   public List<List<Integer>> levelOrder(TreeNode root) {
     List<List<Integer>> res = new ArrayList<>();
