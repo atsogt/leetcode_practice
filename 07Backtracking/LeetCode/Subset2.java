739769507a3590deccccb78b1c7d7fa672966ed9
Anand Tsogtbaatar <atsogt17@gmail.com>
2025-01-09 21:13:36 -0500

    resource added

--- 07Backtracking/LeetCode/Subset2.java
+++ 07Backtracking/LeetCode/Subset2.java
@@ -2,6 +2,25 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/*LeetCode: https://leetcode.com/problems/subsets-ii/description/
+ NeetCode: https://neetcode.io/problems/subsets-ii
+ Concept & Explanation: https://neetcode.io/courses/advanced-algorithms/11
+
+ Given an integer array nums that may contain duplicates, return all possible 
+subsets
+ (the power set).
+
+The solution set must not contain duplicate subsets. Return the solution in any order.
+Example 1:
+
+Input: nums = [1,2,2]
+Output: [[],[1],[1,2],[1,2,2],[2],[2,2]]
+Example 2:
+
+Input: nums = [0]
+Output: [[],[0]]
+ */
+
 public class Subset2 {
 
   public List<List<Integer>> subsetsWithDup(int[] nums) {
