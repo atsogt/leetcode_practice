f42e6bda0bf379270a41fbd70e248a821527f261
Anand Tsogtbaatar <atsogt17@gmail.com>
2025-01-11 23:34:40 -0500

    pop implemnted to heap

--- 08Heap-PriorityQueue/resources/implementation.java
+++ 08Heap-PriorityQueue/resources/implementation.java
@@ -24,6 +24,41 @@ public class implementation {
         i = i / 2;
       }
     }
+
+    public int pop() {
+      if (heap.size() == 1) {
+        // return null;
+      }
+      if (heap.size() == 2) {
+        return heap.remove(heap.size() - 1); // equivalent to heap.remove(1)
+      }
+
+      int res = heap.get(1);
+      // Move last value to root
+      heap.set(1, heap.remove(heap.size() - 1));
+      int i = 1;
+      // Percolate down
+      while (2 * i < heap.size()) {
+        if (2 * i + 1 < heap.size() &&
+            heap.get(2 * i + 1) < heap.get(2 * i) &&
+            heap.get(i) > heap.get(2 * i + 1)) {
+          // Swap right child
+          int tmp = heap.get(i);
+          heap.set(i, heap.get(2 * i + 1));
+          heap.set(2 * i + 1, tmp);
+          i = 2 * i + 1;
+        } else if (heap.get(i) > heap.get(2 * i)) {
+          // Swap left child
+          int tmp = heap.get(i);
+          heap.set(i, heap.get(2 * i));
+          heap.set(2 * i, tmp);
+          i = 2 * i;
+        } else {
+          break;
+        }
+      }
+      return res;
+    }
   }
 
 }
