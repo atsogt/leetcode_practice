daee5d0fed9eb706b6a4046627f9d7f0434d50b1
Anand Tsogtbaatar <atsogt17@gmail.com>
2025-01-11 23:32:10 -0500

    heap implementation

---
+++ 08Heap-PriorityQueue/resources/implementation.java
@@ -0,0 +1,14 @@
+
+public class implementation {
+
+  public class Heap {
+
+    List<Integer> heap;
+
+    public Heap() {
+      heap = new ArrayList<Integer>();
+      heap.add(0);
+    }
+  }
+
+}
