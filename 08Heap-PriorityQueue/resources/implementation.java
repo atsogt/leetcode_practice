d8d3425f1cf54a4ee4ee5656e2e995a85139af4d
Anand Tsogtbaatar <atsogt17@gmail.com>
2025-01-11 23:33:57 -0500

    push implemented to heap

--- 08Heap-PriorityQueue/resources/implementation.java
+++ 08Heap-PriorityQueue/resources/implementation.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class implementation {
 
@@ -8,6 +10,19 @@ public class implementation {
     public Heap() {
       heap = new ArrayList<Integer>();
       heap.add(0);
+    }
+
+    public void push(int val) {
+      heap.add(val);
+      int i = heap.size() - 1;
+
+      // Percolate up
+      while (i > 1 && heap.get(i) < heap.get(i / 2)) {
+        int tmp = heap.get(i);
+        heap.set(i, heap.get(i / 2));
+        heap.set(i / 2, tmp);
+        i = i / 2;
+      }
     }
   }
 
