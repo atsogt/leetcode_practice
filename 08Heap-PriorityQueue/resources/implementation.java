1870b5214de7057b033b9acb969e6d4dd31296f1
Anand Tsogtbaatar <atsogt17@gmail.com>
2025-01-17 23:48:38 -0500

    impelentation

--- 08Heap-PriorityQueue/resources/implementation.java
+++ 08Heap-PriorityQueue/resources/implementation.java
@@ -7,22 +7,10 @@ public class implementation {
 
     List<Integer> heap;
 
+    // implementation
     public Heap() {
       heap = new ArrayList<Integer>();
       heap.add(0);
-    }
-
-    public void push(int val) {
-      heap.add(val);
-      int i = heap.size() - 1;
-
-      // Percolate up
-      while (i > 1 && heap.get(i) < heap.get(i / 2)) {
-        int tmp = heap.get(i);
-        heap.set(i, heap.get(i / 2));
-        heap.set(i / 2, tmp);
-        i = i / 2;
-      }
     }
 
     public int pop() {
