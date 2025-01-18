4469afe7f441bee90de54a4076eb2ddea5653085
Anand Tsogtbaatar <atsogt17@gmail.com>
2025-01-17 23:56:50 -0500

    impelentation

--- 08Heap-PriorityQueue/resources/implementation.java
+++ 08Heap-PriorityQueue/resources/implementation.java
@@ -12,6 +12,21 @@ public class implementation {
       heap = new ArrayList<Integer>();
       heap.add(0);
     }
+
+    // Percolate up
+    public void push(int val) {
+      heap.add(val);
+      int i = heap.size() - 1;
+      // i is less greater one since heap is one indexed
+      // current val is less than parent node in min heap
+      while (i > 1 && heap.get(i) < heap.get(i / 2)) {
+        int temp = heap.get(i);
+        heap.set(i, heap.get(i / 2));
+        heap.set(i / 2, temp);
+        // update index to parent index
+        i = i / 2;
+      }
+    }
   }
 
 }
