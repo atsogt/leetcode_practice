6783eaf7c43e6eda112a4967743eb60e388e7f23
Anand Tsogtbaatar <atsogt17@gmail.com>
2025-01-26 14:50:46 -0500

    max heap implementation

--- 08Heap-PriorityQueue/LeetCode/KClosestPoint/maxHeap.java
+++ 08Heap-PriorityQueue/LeetCode/KClosestPoint/maxHeap.java
@@ -1,5 +1,27 @@
 package KClosestPoint;
 
-public class maxHeap {
+import java.util.PriorityQueue;
 
+public class maxHeap {
+  public class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+      PriorityQueue<int[]> maxHeap = new PriorityQueue<>(
+          (a, b) -> Integer.compare(b[0] * b[0] + b[1] * b[1],
+              a[0] * a[0] + a[1] * a[1]));
+
+      for (int[] point : points) {
+        maxHeap.offer(point);
+        if (maxHeap.size() > k) {
+          maxHeap.poll();
+        }
+      }
+
+      int[][] res = new int[k][2];
+      int i = 0;
+      while (!maxHeap.isEmpty()) {
+        res[i++] = maxHeap.poll();
+      }
+      return res;
+    }
+  }
 }
